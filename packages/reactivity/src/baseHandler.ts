fb3cee15ace89055748ced25ff1d99000df99b7d
shichiye <shichiye@126.com>
2022-07-05 11:42:27 +0800

    feat: update effect

--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -1,14 +1,27 @@
 import { ReactiveFlags } from "./reactive"
-import { activeEffect } from "./effect"
+import { track, trigger } from "./effect"
+import { TrackOpTypes, TriggerOpTypes } from "./operations"
 
 export const mutableHandlers = {
   get(target, key, receiver) {
     if (key === ReactiveFlags.IS_REACTIVE) {
       return true
     }
+
+    // 依赖收集
+    track(target, TrackOpTypes.GET, key)
+
     return Reflect.get(target, key, receiver)
   },
   set(target, key, newValue, receiver) {
-    return Reflect.set(target, key, newValue, receiver)
+
+    let oldValue = target[key]
+    let result = Reflect.set(target, key, newValue, receiver)
+    if (oldValue !== newValue) {
+      // 执行依赖
+      trigger(target, TriggerOpTypes.SET, key, newValue, oldValue)
+    }
+
+    return result
   }
 }
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,9 +1,14 @@
+import { TrackOpTypes, TriggerOpTypes } from "./operations"
 
 export let activeEffect = undefined
+
+const targetMap = new WeakMap()
 
 class ReactiveEffect {
   // 在实例上新增了active属性
   active = true
+  deps = []
+  parent = undefined
   constructor(public fn) {  // 加上 public 后，传入的fn也会加到实例上
     
   }
@@ -16,12 +21,15 @@ class ReactiveEffect {
 
     // 激活状态下，需要执行依赖收集
     try {
+      // 记录当前执行的 activeEffect
+      this.parent = activeEffect
       // 将当前的 effect 和 稍后渲染的属性关联在一起
       activeEffect = this
       // 当稍后调用取值操作的时候，可以获取到全局的 activeEffect，进行依赖收集
       return this.fn()
     } finally {
-      activeEffect = undefined
+      activeEffect = this.parent
+      this.parent = undefined
     }
   }
 }
@@ -31,4 +39,46 @@ export function effect(fn) {
   const _effect = new ReactiveEffect(fn)
 
   _effect.run() // 默认执行一次
+}
+
+
+export function track(target: object, type: TrackOpTypes, key: unknown) {
+  if (!activeEffect) return
+
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()))
+  }
+
+  let shouldTrack = !dep.has(activeEffect)
+
+  if (shouldTrack) {
+    dep.add(activeEffect)
+    // 让 effect 记录对应的属性，以便后续清除
+    activeEffect.deps.push(dep)
+  }
+}
+
+
+export function trigger(
+  target: object,
+  type: TriggerOpTypes,
+  key: unknown,
+  newValue: unknown,
+  oldValue: unknown
+) {
+  const depsMap = targetMap.get(target)
+  if (!depsMap) return
+
+  const effects = depsMap.get(key)
+  
+  effects && effects.forEach(effect => {
+    if (effect !== activeEffect) {
+      effect.run()
+    }
+  })
 }
